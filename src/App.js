fd1832d637c43c49ce924035421de2852003771e
jrtanmoy <roy28301@gmail.com>
2022-07-08 22:33:43 +0600

    some css update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
@@ -10,16 +10,18 @@ import Submit from "./Submit";
 
 function App() {
   return (
-    
-      <Routes>
-      <Route path="/" element={<Step1/>}/>
-      <Route path="/step2" element={<Step2/>}/>
-      <Route path="/step2/step3" element={<Step3/>}/>
-      <Route path="/step2/step3/step4" element={<Step4/>}/>
-      <Route path="/step2/step3/step4/result" element={<Result/>}/>
-      <Route path="/step2/step3/step4/result/submit" element={<Submit/>}/>
-      </Routes>
-    
+    <div className="App">
+      <div className="bg-violet-400	p-10 rounded-xl">
+        <Routes>
+          <Route path="/" element={<Step1 />} />
+          <Route path="/step2" element={<Step2 />} />
+          <Route path="/step2/step3" element={<Step3 />} />
+          <Route path="/step2/step3/step4" element={<Step4 />} />
+          <Route path="/step2/step3/step4/result" element={<Result />} />
+          <Route path="/step2/step3/step4/result/submit" element={<Submit />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
